9e8e26554aa5091974a6adbfb84b1bafe0661dce
MiliSultanaBiswas <149669241+MiliSultanaBiswas@users.noreply.github.com>
2023-12-24 12:54:31 +0530

    Add files via upload

---
+++ arrayRenge12to38.py
@@ -0,0 +1,12 @@
+'''  Write a numpy program to create an '''
+
+import numpy as np   #import numpy
+
+arr=np.arange(12,38)   # create an array with values
+print(arr)    #print the arrayS
+
+#--------------------------------------------------------
+'''   output
+[12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35
+ 36 37]
+'''
