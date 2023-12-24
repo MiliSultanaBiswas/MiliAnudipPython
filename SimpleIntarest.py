123cdc17175fbf56c22665b054daf40271b5aa8e
MiliSultanaBiswas <149669241+MiliSultanaBiswas@users.noreply.github.com>
2023-12-24 10:45:03 +0530

    Add files via upload

---
+++ SimpleIntarest.py
@@ -0,0 +1,11 @@
+''' Write a program to simple intarest on rs 200 for 5 years at 5% per year.'''
+
+principal=200
+intarest=5
+time=5
+simple_intarest=(principal*intarest*time)/100
+print("Simple intarest =",simple_intarest)
+
+''' output
+Simple intarest = 50.0
+'''
