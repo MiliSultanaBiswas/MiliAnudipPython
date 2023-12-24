78a7b0ef5a91195df63f62da0ef7c415e49418a4
MiliSultanaBiswas <149669241+MiliSultanaBiswas@users.noreply.github.com>
2023-12-24 12:55:57 +0530

    Add files via upload

---
+++ StandardDivision.py
@@ -0,0 +1,18 @@
+# complete the standard division of the numpy array input_array=[20,2,7,1,34]
+
+# import numpy
+
+import numpy as np
+input_array=[20,2,7,1,34]
+
+print("array is :",input_array)
+
+print("standerd division is = ",np.std(input_array))
+
+#------------------------------------------------------------------------------------------------------
+# output
+'''
+array is : [20, 2, 7, 1, 34]
+standerd division is =  12.576167937809991
+
+'''
