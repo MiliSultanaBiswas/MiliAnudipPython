9ca523938cfdba3462a3a30be1d2f132ce5bdc52
MiliSultanaBiswas <149669241+MiliSultanaBiswas@users.noreply.github.com>
2023-12-24 10:23:22 +0530

    Add files via upload

---
+++ maximum.py
@@ -0,0 +1,10 @@
+#Declear two variables and print that which variable is largest using ternary...
+
+num1=10
+num2=30
+result="number1 is grater then nuber2" if num1>num2 else "number2 is grater then number1"
+print(result)
+
+''' output
+number2 is grater then number1
+'''
